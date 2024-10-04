b238b48d1aa38a5d11abf546db8ef77b399e7983
alexDev <info@alexandersstudio.uk>
2024-10-04 03:43:23 +0100

    Updated new NEXTJS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-<<<<<<< HEAD
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-};
-=======
 	content: [
 		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -26,23 +9,16 @@ const config: Config = {
 	theme: {
 		extend: {
 			colors: {
-				// Alex color palette
-				alexSky: "#C3EBFA", // Soft blue
-				alexSkyLight: "#EDF9FD", // Lighter blue
-				alexPurple: "#CFCEFF", // Soft purple
-				alexPurpleLight: "#F1F0FF", // Lighter purple
-				alexYellow: "#FAE27C", // Soft yellow
-				alexYellowLight: "#FEFCE8" // Lighter yellow
-			},
-			backgroundImage: {
-				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-				"gradient-conic":
-					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))"
+				alexSky: "#C3EBFA",
+				alexSkyLight: "#EDF9FD",
+				alexPurple: "#CFCEFF",
+				alexPurpleLight: "#F1F0FF",
+				alexYellow: "#FAE27C",
+				alexYellowLight: "#FEFCE8"
 			}
 		}
 	},
 	plugins: []
 };
 
->>>>>>> feature/layout
 export default config;
